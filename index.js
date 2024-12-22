8b1883bbd0c8eb2c44feb14239c9e917bf3e0d3c
Mehedi Zaman <mehedi.zaman.professional@gmail.com>
2024-12-22 13:45:57 +0600

    Basic mongoDb setup

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const cors = require('cors')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
+require('dotenv').config()
+
+const port = process.env.PORT || 5000
+const app = express()
+
+
+
+app.use(cors())
+app.use(express.json())
+app.use(cookieParser())
+
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@main.yolij.mongodb.net/?retryWrites=true&w=majority&appName=Main`
+
+const uri = "mongodb+srv://Job-placement:VXeZLCLtg3SeekPh@cluster0.9yghi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+})
+
+
+async function run() {
+  try {
+console.log('connect')
+  } finally {
+    // Ensures that the client will close when you finish/error
+  }
+}
+run().catch(console.dir)
+app.get('/', (req, res) => {
+  res.send('Hello from SoloSphere Server....')
+})
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
